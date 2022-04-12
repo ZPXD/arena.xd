317e4a429f754af9a3c1c05134c23b2fda53fe94
ZPXD <zajeciaprogramowaniaxd@gmail.com>
2022-04-12 22:14:16 +0200

    Adding notepad minimal solution.

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template
+from flask import request
 
-app=Flask(__name__)
+from flask_wtf import FlaskForm
+
+
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms import (
+    StringField,
+    TextAreaField,
+    SubmitField,
+    PasswordField,
+    DateField,
+    SelectField
+)
+import os
+
+app = Flask(__name__)
+
+app.secret_key = 'super secret key'
 
 
 # Main
@@ -55,6 +72,58 @@ def player_view(match, player):
 def result(match, player):
     return render_template("result.html", match=match, player=player)
 
+
+# Terminal
+
+def save_notepad(file_lines):
+    program_file = 'notepad.py'
+    program_file_backup = 'notepad_last.py'
+
+    output_file = 'output.txt'
+    output_file_backup = 'output_last.txt'
+
+    # Notepad
+    os.system('rm {}'.format(program_file_backup))
+    os.system('cp {} {}'.format(program_file, program_file_backup))
+    os.system('rm {}'.format(program_file))
+
+    # Output
+    os.system('rm {}'.format(output_file_backup))
+    os.system('cp {} {}'.format(output_file, output_file_backup))
+    os.system('rm {}'.format(output_file))
+
+    with open(program_file, 'w+') as f:
+        for line in file_lines:
+            f.write(line)
+    os.system('python3 {} > {}'.format(program_file, output_file))
+
+def read_output():
+    output_file = 'output.txt'
+    with open(output_file, 'r+') as f:
+        output_lines = f.readlines()
+    return output_lines
+
+# Notepad & effect example 
+
+@app.route('/notepad', methods=["GET", "POST"])
+def notepad():
+    form = NotepadForm()
+    if form.validate_on_submit():
+        notepad_text = form.notepad.data
+        notepad_lines = notepad_text.split('\n')
+        save_notepad(notepad_lines)
+    return render_template("terminal.html", form=form)
+
+@app.route('/output', methods=["GET", "POST"])
+def output():
+    output_lines = read_output()
+    return render_template("output.html", output_lines=output_lines)
+
+# Notepad Form
+
+class NotepadForm(FlaskForm):
+    notepad = TextAreaField('Notepad')
+    submit = SubmitField('Submit')
 
 if __name__=="__main__":
     app.run()
---
+++ notepad_last.py
@@ -0,0 +1,1 @@
+print('Sprawdzany')print('Koncept tego co można robić na serwrerze jako player')print('player 1')print('player 2') 
